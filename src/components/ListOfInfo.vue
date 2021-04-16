4389118348b86b37f7a069f20d127ba53c496b1e
jakob <jakobyaro99@gmail.com>
2021-04-16 17:10:38 +0200

    added linkedin page

--- src/components/ListOfInfo.vue
+++ src/components/ListOfInfo.vue
@@ -43,6 +43,7 @@ export default {
       items: [
         { text: 'Ec Utbildningar', icon: 'mdi-clock', link:'https://ecutbildning.se/utbildningar/javautvecklare/stockholm/',id: 0 },
         { text: 'My Resumé', icon: 'mdi-poll', link:'https://www.docdroid.net/PAsdewf/mitt-cvv-pdf',id: 1 },
+        { text: 'Linkedin', icon: 'mdi-linkedin', link:'https://www.docdroid.net/PAsdewf/mitt-cvv-pdf',id: 5 }
         { text: 'Github', icon: 'mdi-palette', link:'https://github.com/jakeHacker99',id: 2 },
         { text: 'Contact', icon: 'mdi-android-messages', link:'https://wa.me/+46793351364?text=tja%20jakob%20jag%20gillade%20din%20VuetifoTodo-app%20;)',id: 2 }
       ],
